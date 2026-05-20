75b710f78f444a7551882142e9636846afe2e581
mdrakibhasan12112 <mdrakibhasan12112@gmail.com>
2026-05-21 04:59:34 +0600

    ganerate token and verify

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -19,6 +20,33 @@ const client = new MongoClient(uri, {
   },
 });
 
+const JWKS = createRemoteJWKSet(new URL('http://localhost:3000/api/auth/jwks'));
+
+// middleware
+const verifyToken =async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({
+        message: 'Unauthorized Access',
+      });
+  }
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({
+      message: 'Invalid Token',
+    });
+  }
+
+try {
+  const { payload } = await jwtVerify(token, JWKS)
+  console.log(payload);
+   next();
+} catch (error) {
+  return res.status(403).json({message:"something wrong"})
+}  
+ 
+};
+
 async function run() {
   try {
     await client.connect();
@@ -26,13 +54,36 @@ async function run() {
 
     const db = client.db('drive-fleet-car');
     const carsCollection = db.collection('explore-car');
-    const bookingCollection = db.collection('bookings')
+    const bookingCollection = db.collection('bookings');
+
+    // app.get('/explore-car', async (req, res) => {
+    //   const result = await carsCollection.find().toArray()
+    //   res.json(result)
+    // })
 
     app.get('/explore-car', async (req, res) => {
-      const result = await carsCollection.find().toArray()
-      res.json(result)
-    })
+      const search = req.query.search || '';
+      const type = req.query.type || '';
 
+      let query = {};
+
+      // search by car name
+      if (search) {
+        query.CarName = {
+          $regex: search,
+          $options: 'i',
+        };
+      }
+
+      // filter by car type
+      if (type) {
+        query.carType = type;
+      }
+
+      const result = await carsCollection.find(query).toArray();
+
+      res.json(result);
+    });
 
     // find all data in explore-car
     app.post('/explore-car', async (req, res) => {
@@ -42,53 +93,54 @@ async function run() {
       res.json(result);
     });
 
-    app.get('/explore-car/:id', async (req, res) => {
-      const { id } = req.params
-      const result = await carsCollection.findOne({ _id: new ObjectId(id) })
-      res.json(result)
-    })
+    // middlewire
+    app.get('/explore-car/:id', verifyToken, async (req, res) => {
+      const { id } = req.params;
+      const result = await carsCollection.findOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
 
     // edit car model
     app.patch('/explore-car/:id', async (req, res) => {
-      const { id } = req.params
+      const { id } = req.params;
       const updatedData = req.body;
 
-      const result =await carsCollection.updateOne(
+      const result = await carsCollection.updateOne(
         { _id: new ObjectId(id) },
-        {$set: updatedData}
-      )
-      res.json(result)
-    })
+        { $set: updatedData },
+      );
+      res.json(result);
+    });
 
     // delete car data
     app.delete('/explore-car/:id', async (req, res) => {
-      const { id } = req.params
-      const result = await carsCollection.deleteOne({ _id: new ObjectId(id) })
-      res.json(result)
-    })
+      const { id } = req.params;
+      const result = await carsCollection.deleteOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
 
     // sent booking data in forntend
     app.get('/my-bookings/:userId', async (req, res) => {
-      const { userId } = req.params
-      const result = await bookingCollection.find({ userId: userId }).toArray()
-      res.json(result)
-    })
-
+      const { userId } = req.params;
+      const result = await bookingCollection.find({ userId: userId }).toArray();
+      res.json(result);
+    });
 
     // recive booking data
     app.post('/my-bookings', async (req, res) => {
       const bookingData = req.body;
-      const result = await bookingCollection.insertOne(bookingData)
-      res.json(result)
-    })
+      const result = await bookingCollection.insertOne(bookingData);
+      res.json(result);
+    });
 
     // delete booking card
     app.delete('/my-bookings/:bookingId', async (req, res) => {
       const { bookingId } = req.params;
-      const result = await bookingCollection.deleteOne({ _id: new ObjectId(bookingId) });
-      res.json(result)
-    })
-
+      const result = await bookingCollection.deleteOne({
+        _id: new ObjectId(bookingId),
+      });
+      res.json(result);
+    });
 
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!',
